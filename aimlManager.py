f0e48c03655ac5bfe938fe3f624ca4f0d8c9a0f4
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2017-09-21 20:50:28 -0300

    Initial commit

---
+++ aimlManager.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import aiml
+import time
+import random
+
+class aimlManager():
+    
+    # Create the kernel and learn AIML files
+    
+	def __init__(self):
+		self.kernel = aiml.Kernel()
+		self.kernel.learn("script.aiml")
+		print(self.kernel.respond("START"))
+		
+	def mensagem(self, text):
+		return self.kernel.respond(text)
---
+++ local.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-	
+import sys
+import aimlManager
+
+aimlMgr = aimlManager.aimlManager()
+
+while True:
+
+	text = sys.stdin.readline()
+	text = text.strip()
+	
+	if text == "quit":	
+		print("---------------")
+		print("- Fim de chat")
+		print("---------------")
+		break
+
+	resposta = aimlMgr.mensagem(text)
+	resposta = resposta.replace('\\n','\n')
+	if not resposta == "":
+		print(">> " + resposta)
+		print("---------------")
